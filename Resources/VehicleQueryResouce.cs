1b02919623e6c087f7d4578f7260304f55d4ea82
Kumail Jawadi <mk.jawadi@hotmail.com>
2018-02-27 10:57:27 +0430

    Implement Sorting in server-side plus code refactring.

--- Resources/VehicleQueryResouce.cs
+++ Resources/VehicleQueryResouce.cs
@@ -3,5 +3,8 @@ namespace vega.Resources
     public class VehicleQueryResouce
     {
         public int? MakeId { get; set; }
+        public int? ModelId { get; set; }
+        public string SortBy { get; set; }
+        public bool IsSortAscending { get; set; }
     }
 }
